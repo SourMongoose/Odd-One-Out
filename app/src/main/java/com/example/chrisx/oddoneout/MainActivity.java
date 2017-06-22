e6342cebdb62e859af4531389b571475183bf00b
Chris Xiong <chrisxiongyang@yahoo.com>
2017-06-21 20:37:56 -0700

    Made app fullscreen
    Hide status bar

--- app/src/main/java/com/example/chrisx/oddoneout/MainActivity.java
+++ app/src/main/java/com/example/chrisx/oddoneout/MainActivity.java
@@ -25,6 +25,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 
@@ -66,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         //hide app preview in task manager (anti-cheat)
